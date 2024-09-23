dcff1f7f1bc65fe941f78b06635d5abc20e1c8a2
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2024-09-23 22:44:02 +0530

    init repo + create api to extract data

---
+++ app.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from ingestion import main
+
+app = FastAPI()
+
+@app.post("/add-data")
+async def add_data(request: Request):
+    data = await request.json()
+    url = data.get('url')
+    if url:
+        response = main(url)
+        return {"message": response}
+    else:
+        return {"message": "No URL provided"}
+
+if __name__ == "__main__":
+    uvicorn.run(app)
