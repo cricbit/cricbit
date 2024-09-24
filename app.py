81c310172ebf17078e517d84a79cc2fe57100728
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2024-09-24 22:28:43 +0530

    use background worker for data ingestion api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 from ingestion import main
 
 app = FastAPI()
@@ -9,12 +9,12 @@ async def root():
     return {"Status": "OK"}
 
 @app.post("/add-data")
-async def add_data(request: Request):
+async def add_data(request: Request, background_tasks: BackgroundTasks):
     data = await request.json()
     url = data.get('url')
     if url:
-        response = main(url)
-        return {"message": response}
+        background_tasks.add_task(main, url)
+        return {"message": "Data ingestion started in the background"}
     else:
         return {"message": "No URL provided"}
 
