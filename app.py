f563a879906d5f8c42fa22f02b446df896f3e01b
Prateek Bhardwaj <prateek.bhardwaj@quizizz.com>
2024-09-25 18:24:37 +0530

    move background tasks out of try catch block

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ async def add_data(request: Request, background_tasks: BackgroundTasks):
     try:
         data = await request.json()
         url = data.get('url')
-        if url:
-            background_tasks.add_task(main, url)
-            return {"message": "Data ingestion started in the background"}
-        else:
-            return {"message": "No URL provided"}
     except Exception as e:
         return {"error": str(e)}
+    if url:
+        background_tasks.add_task(main, url)
+        return {"message": "Data ingestion started in the background"}
+    else:
+        return {"error": "No URL provided"}
 
 if __name__ == "__main__":
     uvicorn.run(app)
