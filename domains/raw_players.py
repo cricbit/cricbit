19187d0e21e647d4a82a3e066b5d222bdfb8ec1f
Prateek Bhardwaj <prateekb1912@gmail.com>
2025-03-28 02:43:10 +0530

    remove raw players

--- domains/raw_players.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Integer
-from domains.base import Base
-
-class RawPlayer(Base):
-    __tablename__ = 'raw_players'
-
-    player_id = Column(String, primary_key=True)
-    name = Column(String, nullable=False)
-    cricinfo_id = Column(Integer, nullable=True, unique=True)
